922a346d54db1cd49b890b520ff373900793dfea
aaroncox <aaron.cox@greymass.com>
2026-02-03 13:14:44 -0800

    added prefix scan and state reporting to --inspect

--- services/actionindex/internal/inspect.go
+++ services/actionindex/internal/inspect.go
@@ -1,10 +1,12 @@
 package internal
 
 import (
+	"encoding/binary"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/cockroachdb/pebble/v2"
 	"github.com/cockroachdb/pebble/v2/bloom"
@@ -129,6 +131,129 @@ func inspectOptionsFile(filename string) {
 	logger.Println("startup", "")
 }
 
+var knownPrefixes = map[byte]string{
+	PrefixAccountActions:       "AccountActions",
+	PrefixContractAction:       "ContractAction",
+	PrefixContractWildcard:     "ContractWildcard",
+	PrefixProperties:           "Properties",
+	PrefixWAL:                  "WAL",
+	PrefixTimeMap:              "TimeMap",
+}
+
+func inspectPrefixes(db *pebble.DB) {
+	logger.Printf("startup", "Key prefix scan:")
+
+	for pfx := 0; pfx < 256; pfx++ {
+		lower := []byte{byte(pfx)}
+		var upper []byte
+		if pfx < 255 {
+			upper = []byte{byte(pfx + 1)}
+		}
+
+		iter, err := db.NewIter(&pebble.IterOptions{
+			LowerBound: lower,
+			UpperBound: upper,
+		})
+		if err != nil {
+			logger.Printf("startup", "  0x%02X: error creating iterator: %v", pfx, err)
+			continue
+		}
+
+		var keyCount uint64
+		var valueBytes uint64
+		for iter.First(); iter.Valid(); iter.Next() {
+			keyCount++
+			valueBytes += uint64(len(iter.Value()))
+		}
+		if err := iter.Error(); err != nil {
+			logger.Printf("startup", "  0x%02X: iterator error: %v", pfx, err)
+		}
+		iter.Close()
+
+		if keyCount == 0 {
+			continue
+		}
+
+		name := knownPrefixes[byte(pfx)]
+		if name != "" {
+			logger.Printf("startup", "  0x%02X %-22s %d keys, %s values",
+				pfx, name, keyCount, logger.FormatBytes(int64(valueBytes)))
+		} else {
+			logger.Printf("startup", "  0x%02X %-22s %d keys, %s values",
+				pfx, "(unknown)", keyCount, logger.FormatBytes(int64(valueBytes)))
+		}
+	}
+	logger.Println("startup", "")
+}
+
+func inspectSyncState(db *pebble.DB) {
+	logger.Printf("startup", "Sync state:")
+
+	propsKey := makePropertiesKey()
+	if val, closer, err := db.Get(propsKey); err == nil {
+		if libNum, headNum, ok := parsePropertiesValue(val); ok {
+			logger.Printf("startup", "  LIB block: %d", libNum)
+			logger.Printf("startup", "  Head block: %d", headNum)
+		}
+		closer.Close()
+	} else {
+		logger.Printf("startup", "  No properties key found")
+	}
+
+	tmLower := []byte{PrefixTimeMap}
+	tmUpper := []byte{PrefixTimeMap + 1}
+	iter, err := db.NewIter(&pebble.IterOptions{
+		LowerBound: tmLower,
+		UpperBound: tmUpper,
+	})
+	if err != nil {
+		logger.Printf("startup", "  TimeMap iterator error: %v", err)
+		logger.Println("startup", "")
+		return
+	}
+	defer iter.Close()
+
+	var count int
+	var firstHour, lastHour uint32
+	var firstMinSeq, lastMaxSeq uint64
+
+	for iter.First(); iter.Valid(); iter.Next() {
+		hour, ok := parseTimeMapKey(iter.Key())
+		if !ok {
+			continue
+		}
+		val := iter.Value()
+		if len(val) != 16 {
+			continue
+		}
+		minSeq := binary.BigEndian.Uint64(val[0:8])
+		maxSeq := binary.BigEndian.Uint64(val[8:16])
+
+		if count == 0 {
+			firstHour = hour
+			firstMinSeq = minSeq
+		}
+		lastHour = hour
+		lastMaxSeq = maxSeq
+		count++
+	}
+
+	if count > 0 {
+		firstTime := time.Unix(int64(firstHour)*3600, 0).UTC()
+		lastTime := time.Unix(int64(lastHour)*3600, 0).UTC()
+		duration := lastTime.Sub(firstTime)
+
+		logger.Printf("startup", "  TimeMap entries: %d", count)
+		logger.Printf("startup", "  First hour: %s (seq %d)", firstTime.Format("2006-01-02 15:04 UTC"), firstMinSeq)
+		logger.Printf("startup", "  Last hour:  %s (seq %d)", lastTime.Format("2006-01-02 15:04 UTC"), lastMaxSeq)
+		logger.Printf("startup", "  Duration: %s (%.1f days)", duration, duration.Hours()/24)
+		logger.Printf("startup", "  Sequence range: %d → %d (%d total)", firstMinSeq, lastMaxSeq, lastMaxSeq-firstMinSeq)
+	} else {
+		logger.Printf("startup", "  No TimeMap entries found")
+	}
+	logger.Println("startup", "")
+}
+
 func inspectLiveMetrics(path string) {
 	snappyFn := func() *block.CompressionProfile { return block.SnappyCompression }
 
@@ -153,6 +278,9 @@ func inspectLiveMetrics(path string) {
 		return
 	}
 	defer db.Close()
+
+	inspectPrefixes(db)
+	inspectSyncState(db)
 
 	m := db.Metrics()
 	logger.Printf("startup", "Pebble metrics:")
